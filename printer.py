b9e8369f039105071c1715919ca2a86465d6aa43
panderiole <335097@niuitmo.ru>
2022-10-16 05:49:34 +0600

    write "1" and test)
    Took 3 minutes

---
+++ printer.py
@@ -0,0 +1,55 @@
+import dataclasses
+from enum import Enum
+
+from models import Tasks
+
+
+@dataclasses.dataclass(frozen=True)
+class AllowedCommands(Enum):
+    SHOW_TODAY_TASKS = "1"
+    ADD_TASKS = "2"
+    EDIT_TASKS = "3"
+    STATS = "4"
+    SHOW_TASKS_BY_DATE = "5"
+
+
+def detect_command(user_answer: str) -> AllowedCommands | None:
+    for command in AllowedCommands:
+        if user_answer == command.value:
+            return command
+    return None
+
+
+def print_hi_message() -> str:
+    return input("Привет! Выбери команду: ").strip()
+
+
+def print_hi_error_message() -> str:
+    print("Ошибка ввода!")
+    return print_hi_message()
+
+
+def format_tasks(tasks: Tasks) -> str:  # in future add uncompleted task retriever for the current day
+    message = f"%SEPARATOR%\n| Группа задач №{tasks.id}\n| Задачи на {tasks.for_what_date.to_string()}\n%SEPARATOR%"
+    max_length = 0
+    for t in tasks.tasks:
+        part_1 = f"\n| №{t.id}: [{t.start_time.to_string()} - {t.finish_time.to_string()}] {t.name}\n"
+        part_2 = f"| Описание: {t.description}\n"
+        part_3 = f"| Статус выполнения: {t.is_completed}\n"
+        max_l = max(max(len(part_1), len(part_2)), len(part_3))
+        if max_length < max_l:
+            max_length = max_l
+        message += part_1 + part_2 + part_3 + "%SEPARATOR%"
+    message = message.replace("%SEPARATOR%", '+' + (max_length - 1) * '-' + '+')
+
+    new_message = ''
+    for line in message.split('\n'):
+        if line[-1] != '+':
+            new_message = new_message + line + (max_length - len(line)) * ' ' + "|\n"
+        else:
+            new_message = new_message + line + '\n'
+    return new_message
+
+
+def print_today_tasks(message: str) -> str:
+    return input(message)
---
+++ tasks_storage.py
@@ -0,0 +1,91 @@
+import json
+from pathlib import Path
+
+from models import *
+
+
+def read_tasks_by_id(tasks_id: int) -> Tasks | None:
+    data = _read_from_file(tasks_id)
+    if not data:
+        return None
+    tasks = _parse_data_from_file(data)
+    return tasks
+
+
+def write_tasks_update(tasks: Tasks):
+    pass
+
+
+def write_new_tasks(tasks: Tasks):
+    pass
+
+
+def load_storage_preview() -> StoragePreview:
+    index = 0
+    data = []
+    while True:
+        index += 1
+        task = read_tasks_by_id(index)
+        if task:
+            data.append(FileObj(id=task.id, filename=f"{index}.json", created_for=task.for_what_date))
+        else:
+            break
+    return StoragePreview(data)
+
+
+def _read_from_file(tasks_id: int) -> str | None:
+    file_path = f"storage/{tasks_id}.json"
+    if Path(file_path).is_file():
+        file = open(file=file_path, mode='r', encoding='UTF-8')
+        data = file.read()
+        file.close()
+        return data
+    return None
+
+
+def _parse_data_from_file(data: str) -> Tasks:
+    try:
+        js_data = json.loads(data)
+        tasks_list = []
+        for js_task in js_data['tasks']:
+            tasks_list.append(Task(
+                id=js_task['id'],
+                name=js_task['name'],
+                description=js_task['description'],
+                start_time=TimeFormat(
+                    hours=int(js_task['start_time'].split(TimeFormat.separator)[0]),
+                    minutes=int(js_task['start_time'].split(TimeFormat.separator)[1])
+                ),
+                finish_time=TimeFormat(
+                    hours=int(js_task['finish_time'].split(TimeFormat.separator)[0]),
+                    minutes=int(js_task['finish_time'].split(TimeFormat.separator)[1])
+                ),
+                is_completed=js_task['is_completed'],
+                creation_date=DateFormat(
+                    day=int(js_task['creation_date'].split(DateFormat.separator)[0]),
+                    month=int(js_task['creation_date'].split(DateFormat.separator)[1]),
+                    year=int(js_task['creation_date'].split(DateFormat.separator)[2])
+                )
+            ))
+        return Tasks(
+            id=js_data['id'],
+            tasks=tasks_list,
+            for_what_date=DateFormat(
+                day=int(js_data['for_what_date'].split(DateFormat.separator)[0]),
+                month=int(js_data['for_what_date'].split(DateFormat.separator)[1]),
+                year=int(js_data['for_what_date'].split(DateFormat.separator)[2]),
+            ))
+    except Exception:
+        print("err")
+
+
+def _convert_to_json():
+    pass
+
+
+def _update_storage_file():
+    pass
+
+
+if __name__ == "__main__":
+    print(read_tasks_by_id(1))
